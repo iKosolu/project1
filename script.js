ad9555080f091298b71e9e115656e712d1c6139a
iKosolu <kp.madueke@stu.unizik.edu.ng>
2023-02-07 15:51:51 +0100

    some edits

--- script.js
+++ script.js
@@ -2,83 +2,75 @@
 
 // Creating a random whole number and scoreCount
 var secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
+// console.log(secretNumber);
 var scoreCount = 20;
 var highscore = 0;
+
+// functions
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function setBGcolor(color) {
+  document.querySelector('body').style.backgroundColor = color;
+}
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   //   When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🛑 No Number!';
+    displayMessage('🛑 No Number!');
   }
+
   // When the guess is correct
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '✅ Correct Number';
-
-    document.querySelector('body').style.backgroundColor = '#60b347';
+    displayMessage('✅ Correct Number');
+    setBGcolor('#60b347');
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
 
+    // highscore
     if (scoreCount > highscore) {
       highscore = scoreCount;
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    // to disappear after
-    // document.querySelector('.guess').style.display = 'none';
+    // Input box to disappear after guess is correct
     document.querySelector('.guess').style.visibility = 'hidden';
   }
-  //   When guess is lower than random number
-  else if (guess < secretNumber) {
+
+  // When guess is NOT correct
+  else if (guess !== secretNumber) {
     if (scoreCount > 1) {
-      document.querySelector('.message').textContent = '📉 Low';
+      displayMessage(guess < secretNumber ? '📉 Low' : '📈 High');
       scoreCount = scoreCount - 1;
       console.log(scoreCount);
       document.querySelector('.score').textContent = scoreCount;
     } else {
       scoreCount = scoreCount - 1;
       document.querySelector('.score').textContent = 0;
-      document.querySelector('.message').textContent = '😞 You Lost the game';
+      displayMessage('😞 You Lost the game');
 
-      document.querySelector('body').style.backgroundColor = 'red';
+      setBGcolor('red');
       // to disappear after
       // document.querySelector('.guess').style.display = 'none';
       document.querySelector('.guess').style.visibility = 'hidden';
     }
   }
-  //   When guess is higher than random number
-  else if (guess > secretNumber) {
-    if (scoreCount > 1) {
-      document.querySelector('.message').textContent = '📈 High';
-      scoreCount = scoreCount - 1;
-      console.log(scoreCount);
-      document.querySelector('.score').textContent = scoreCount;
-    } else {
-      scoreCount = scoreCount - 1;
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('.message').textContent =
-        '😞 You Lost, try again!';
 
-      document.querySelector('body').style.backgroundColor = 'red';
-      // to disappear after
-      // document.querySelector('.guess').style.display = 'none';
-      document.querySelector('.guess').style.visibility = 'hidden';
-    }
-  }
   //   default
   else {
-    document.querySelector('.message').textContent = '🟡 Keep Guessing';
+    displayMessage('🟡 Keep Guessing');
   }
 });
 
-// Reset
+// Reset Button
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   scoreCount = 20;
-  console.log(secretNumber);
-  document.querySelector('body').style.backgroundColor = '#222';
+  // console.log(secretNumber);
+  setBGcolor('#222');
   document.querySelector('.guess').style.visibility = 'visible';
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('.number').style.width = '20rem';
